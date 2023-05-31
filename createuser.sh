8e7a5964d344c1de7b3d66ca1ad08ffe96ee7d03
Abiodun Babalola <babalolaaa@yahoo.com>
2023-05-31 15:11:57 -0400

    1st commit

---
+++ createuser.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script requires root/sudo access
+#
+echo "Please enter the name of the newUser"
+read newuser
+echo "##############Now ready to create $newuser account################# "
+sudo adduser  $newuser
+echo "$newuser account is created successfully"
+grep $newuser /etc/passwd # to verify
+echo "##############Now ready to create $newuser password################# "
+sudo passwd  $newuser
+echo "$newuser password is created successfully"
+
+
+
